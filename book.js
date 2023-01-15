f41866e7558fdfacf407b3c346ab9ffd7d3158d0
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-14 19:51:10 -0800

    Add function to add books to HTML

--- book.js
+++ book.js
@@ -1,4 +1,6 @@
-function book(title, author, pages, read) {
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -14,5 +16,32 @@ function book(title, author, pages, read) {
     };
 };
 
-theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+function addBookToLibrary(myBook) {
+    myLibrary.push(myBook);
+};
+
+const bookshelf = document.getElementById('bookshelf');
+
+function displayBooks() {
+    library_length = myLibrary.length;
+    for (let i = 0; i < library_length; i++) {
+        let shelveBook = document.createElement('div');
+        shelveBook.textContent = myLibrary[i].title;
+        bookshelf.appendChild(shelveBook);
+    }
+};
+
+
+
+theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary(theHobbit);
+
+theIdiot = new Book("The Idiot", "Fyodor Dostoevsky", 768, true)
+addBookToLibrary(theIdiot);
+
+theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 110, true);
+addBookToLibrary(theGreatGatsby);
+
+console.log(myLibrary);
+
+displayBooks();
